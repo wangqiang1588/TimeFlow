497ba02ffa297a4a9ab49531456e8a27463dfc40
默默 <wangqiang1588@sina.com>
2017-08-13 20:57:27 +0800

    Add New Time Activity

---
+++ timeflowmgr-biz/src/main/java/com/mobibrw/timeflowmgr/biz/Activity/TimeFlowAddItemActivity.java
@@ -0,0 +1,15 @@
+package com.mobibrw.timeflowmgr.biz.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.mobibrw.timeflowmgr.biz.R;
+
+public class TimeFlowAddItemActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_time_flow_add_item);
+    }
+}
