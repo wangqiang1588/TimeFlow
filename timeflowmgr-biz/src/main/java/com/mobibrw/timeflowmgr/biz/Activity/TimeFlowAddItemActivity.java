8307a120b027b993fda7d22f5f2dd283b51248d2
默默 <wangqiang1588@sina.com>
2017-08-14 18:48:59 +0800

    improve time item add windows

--- timeflowmgr-biz/src/main/java/com/mobibrw/timeflowmgr/biz/Activity/TimeFlowAddItemActivity.java
+++ timeflowmgr-biz/src/main/java/com/mobibrw/timeflowmgr/biz/Activity/TimeFlowAddItemActivity.java
@@ -2,6 +2,12 @@ package com.mobibrw.timeflowmgr.biz.Activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TimePicker;
+import android.widget.Toast;
 
 import com.mobibrw.timeflowmgr.biz.R;
 
@@ -11,5 +17,29 @@ public class TimeFlowAddItemActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_time_flow_add_item);
+        final Button btnCommit = (Button)findViewById(R.id.btnCommit);
+        btnCommit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setAndSaveTimeFlowItems();
+            }
+        });
+    }
+
+    private void setAndSaveTimeFlowItems(){
+        final EditText editText = (EditText)findViewById(R.id.editContent);
+        final String strContent = editText.getText().toString();
+        if((null == strContent) || (strContent.length() <= 0 )){
+            Toast.makeText(this.getBaseContext(),this.getString(R.string.action_settings),Toast.LENGTH_SHORT).show();
+        }else {
+            final DatePicker datePicker = (DatePicker) findViewById(R.id.datePicker);
+            final int year = datePicker.getYear();
+            final int month = datePicker.getMonth();
+            final int day = datePicker.getDayOfMonth();
+            final TimePicker timePicker = (TimePicker) findViewById(R.id.timePicker);
+            final int hour = timePicker.getCurrentHour();
+            final int minute = timePicker.getCurrentMinute();
+            TimeFlowAddItemActivity.this.finish();
+        }
     }
 }
