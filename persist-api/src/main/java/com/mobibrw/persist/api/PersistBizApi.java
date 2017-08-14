32b365c67afb303a6b083936dd4d730a7b322fb0
默默 <wangqiang1588@sina.com>
2017-08-14 23:02:44 +0800

    Add Persist Biz Logic

---
+++ persist-api/src/main/java/com/mobibrw/persist/api/PersistBizApi.java
@@ -0,0 +1,19 @@
+package com.mobibrw.persist.api;
+
+import com.mobibrw.lego.LegoApiBundle;
+
+/**
+ * Created by longsky on 2017/8/14.
+ */
+
+public class PersistBizApi extends LegoApiBundle {
+    @Override
+    protected void onBundleCreate() {
+
+    }
+
+    @Override
+    protected void onBundleDestroy() {
+
+    }
+}
---
+++ persist-biz/src/main/java/com/mobibrw/persist/biz/PersistBiz.java
@@ -0,0 +1,19 @@
+package com.mobibrw.persist.biz;
+
+import com.mobibrw.lego.LegoBizBundle;
+
+/**
+ * Created by longsky on 2017/8/14.
+ */
+
+public class PersistBiz extends LegoBizBundle {
+    @Override
+    protected void onBundleCreate() {
+
+    }
+
+    @Override
+    protected void onBundleDestroy() {
+
+    }
+}
